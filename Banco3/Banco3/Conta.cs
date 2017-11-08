74a9d75d42657db56022c4a5fb803ba7124d5539
Rodrigo Tenorio de Boa Ventura <boaventura19@yahoo.com.br>
2017-11-08 11:28:05 -0200

    Feita criacao de Metodos(Transfere,Saca,Deposisto) na class Conta.

--- Banco3/Banco3/Conta.cs
+++ Banco3/Banco3/Conta.cs
@@ -9,11 +9,43 @@ namespace Banco3
     class Conta
     {
         public static int Id { get; private set; }
-        public double Saldo { get; set; }
+        public double Saldo { get;private set; }
 
         public Conta()
         {
             Conta.Id++;
+        }
+
+        public bool Saca(double valor)
+        {
+            if (this.Saldo >= valor)
+            {
+                this.Saldo -= valor;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Deposito(double valor)
+        {
+            this.Saldo += valor;
+        }
+
+        public bool Transfere(double valor,Conta destino)
+        {
+            if (this.Saldo >= valor)
+            {
+                this.Saldo -= valor;
+                destino.Deposito(valor);  // destino.Saldo += valor;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     };
 }
